188b9151b6df7da02391a6edb910b322c157dbb2
Pekka Tiitinen <pekka-1992@hotmail.com>
2023-12-27 13:56:58 +0200

    day7 part 2

--- day7/advent.py
+++ day7/advent.py
@@ -3,22 +3,25 @@ from collections import Counter
 with open('input.txt') as f:
     hands = [hand.strip().split(' ') for hand in f.readlines()]
     card_strengths = ['2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A']
-    card_strengths2 = ['2', '3', '4', '5', '6', '7', '8', '9', 'T', 'Q', 'K', 'A', 'J']
+    card_strengths2 = ['J', '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'Q', 'K', 'A']
 
 
 def rank_hand(hand, use_jokers):
     jokers = hand.count('J')
-    has_joker = jokers > 0
     card_count = Counter(hand)
     most_common_counts = [list(i) for i in card_count.most_common()]
     score = 0
 
     if use_jokers and jokers < 5:
-        most_common_counts[0][1] += jokers
+        if most_common_counts[0][0] != 'J':
+            most_common_counts[0][1] += jokers
+        else:
+            most_common_counts[1][1] += jokers
+            most_common_counts = sorted(most_common_counts, reverse=True,key=lambda x: x[1])
+        most_common_counts = [card_set for card_set in most_common_counts if card_set[0] != 'J']
 
-    print(hand, most_common_counts)
     if most_common_counts[0][1] >= 5:
-        score = 6 if jokers < 5 else 5 - jokers / 10
+        score = 6
     elif most_common_counts[0][1] == 4:
         score = 5
     elif most_common_counts[0][1] == 3:
@@ -38,7 +41,7 @@ def rank_hand(hand, use_jokers):
 
 hands1 = sorted(hands, key=lambda hand: (rank_hand(hand[0], False), [card_strengths.index(card) for card in hand[0]]))
 hands2 = sorted(hands, key=lambda hand: (rank_hand(hand[0], True), [card_strengths2.index(card) for card in hand[0]]))
-print(hands2)
+
 result1 = 0
 for i, hand in enumerate(hands1):
     rank = i + 1
